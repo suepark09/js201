98f713872ebc55e4cffb591d9c4fb2e3a52412d8
Sue Park <suepandrice06@gmail.com>
2019-06-26 09:14:08 -0500

    Exercise 201

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -2,9 +2,18 @@
 // string that says hello to the name.
 // Example: hello('Mustache') should return 'Hello, Mustache!'
 
-
+function hello (name) {
+    return "Hello, " + name + "!"
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+function helloDefault (name) {
+    if (name) {
+        return "Hello, " + name + "!"
+    } else {
+        return "Hello, world!"
+    }
+}
